4297d01c9e7e796291c28cbb4dba7216e96814da
Alessandro Perlo <aleperlo002@protonmail.com>
2021-12-07 10:41:14 +0100

    added day 7

---
+++ day7/main.py
@@ -0,0 +1,25 @@
+with open("input", "r") as tmp_file:
+    crabs_pos = [int(i) for i in tmp_file.readline().split(",")]
+
+# part 1
+pos_max = max(crabs_pos)
+crabs = [crabs_pos.count(i) for i in range(pos_max + 1)]
+
+fuel = [0 for i in range(pos_max + 1)]
+
+for i in range(pos_max + 1):
+    for c in range(pos_max + 1):
+        fuel[i] += abs(c - i) * crabs[c]
+
+print(min(fuel))
+
+# part 2
+
+fuel = [0 for i in range(pos_max + 1)]
+
+for i in range(pos_max + 1):
+    for c in range(pos_max + 1):
+        exp = sum([e for e in range(0, abs(c - i) + 1)])
+        fuel[i] += exp * crabs[c]
+
+print(min(fuel))
